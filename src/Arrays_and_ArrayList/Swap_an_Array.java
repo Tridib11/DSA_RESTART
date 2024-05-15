0c0cae3f3d0b2fad0f96f2ac0c61c14d5caa2dc3
Tridib11 <tridibghosh12345@gmail.com>
2024-05-15 16:48:17 +0530

    Files added

--- src/Arrays_and_ArrayList/Swap_an_Array.java
+++ src/Arrays_and_ArrayList/Swap_an_Array.java
@@ -1,5 +1,23 @@
 package Arrays_and_ArrayList;
 
-public class Swap_an_Array {
+import java.util.Arrays;
 
+public class Swap_an_Array {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int start=0;
+        int end=arr.length-1;
+        reverse(arr,start,end);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr,int start,int end){
+        while(start<=end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
 }
