ce8fef408556055253ac9527529432cd6c50304e
Tridib11 <tridibghosh12345@gmail.com>
2024-05-16 14:56:42 +0530

    Files added

--- src/Arrays_and_ArrayList/Find_the_number_with_Even_number_of_digits.java
+++ src/Arrays_and_ArrayList/Find_the_number_with_Even_number_of_digits.java
@@ -4,6 +4,11 @@ public class Find_the_number_with_Even_number_of_digits {
 
     public static void main(String[] args) {
         int[] arr={12,345,2,6,7896};
+        int ans=findNumbers(arr);
+        System.out.println(ans);
+    }
+
+    public static int findNumbers(int[] nums){
 
     }
 }
