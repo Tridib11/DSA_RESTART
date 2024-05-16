d28a3503abec08579cff37b8e8bca156c6f2edeb
Tridib11 <tridibghosh12345@gmail.com>
2024-05-16 15:36:07 +0530

    Files added

---
+++ src/Arrays_and_ArrayList/Richest_Customer_Wealth.java
@@ -0,0 +1,11 @@
+package Arrays_and_ArrayList;
+
+public class Richest_Customer_Wealth {
+    public static void main(String[] args) {
+
+    }
+
+    public int maximumWealth(int[][] accounts) {
+
+    }
+}
