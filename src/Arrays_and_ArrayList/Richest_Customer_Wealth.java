50090ab6c0adffcab0bfeb65ce8950263c2fdc2e
Tridib11 <tridibghosh12345@gmail.com>
2024-05-16 15:57:07 +0530

    Files added

--- src/Arrays_and_ArrayList/Richest_Customer_Wealth.java
+++ src/Arrays_and_ArrayList/Richest_Customer_Wealth.java
@@ -2,11 +2,25 @@ package Arrays_and_ArrayList;
 
 public class Richest_Customer_Wealth {
     public static void main(String[] args) {
-
+        int[][] accounts={
+                {1,2,3},
+                {3,2,1}
+        };
+        int ans=maximumWealth(accounts);
+        System.out.println(ans);
     }
 
-    public int maximumWealth(int[][] accounts) {
-        int maxWealth=0;
-
+    public static int maximumWealth(int[][] accounts) {
+        int maxWealth=Integer.MIN_VALUE;
+        for(int rows=0;rows<accounts.length;rows++){
+            int sum=0;
+            for(int coloumns=0;coloumns<accounts[rows].length;coloumns++){
+                sum+=accounts[rows][coloumns];
+            }
+            if(sum>maxWealth){
+                maxWealth=sum;
+            }
+        }
+        return maxWealth;
     }
 }
