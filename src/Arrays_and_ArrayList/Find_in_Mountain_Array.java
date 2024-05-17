e21801cee042e0921ee2368f7973d995c4b0f116
Tridib11 <tridibghosh12345@gmail.com>
2024-05-17 23:55:08 +0530

    Files added

--- src/Arrays_and_ArrayList/Find_in_Mountain_Array.java
+++ src/Arrays_and_ArrayList/Find_in_Mountain_Array.java
@@ -1,8 +1,19 @@
 package Arrays_and_ArrayList;
 
+import java.util.*;
+
+interface MountainArray {
+    public int get(int index){}
+
+    public int length(){}
+}
+
 public class Find_in_Mountain_Array {
-    interface MountainArray{
-        public int get(int index){}
-        public int length(){}
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peak=getPeakElement(mountainArr);
+    }
+
+    int getPeakElement(MountainArray mountainArr){
+        
     }
 }
---
+++ src/Arrays_and_ArrayList/Order_Agnostic_Binary_Search.java
@@ -0,0 +1,4 @@
+package Arrays_and_ArrayList;
+
+public class Order_Agnostic_Binary_Search {
+}
