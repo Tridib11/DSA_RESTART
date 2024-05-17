0aee82acc14a2f6d39bcb2bf6253a7cb6ab754c7
Tridib11 <tridibghosh12345@gmail.com>
2024-05-18 00:31:28 +0530

    Add BinarySearch and Find_in_Mountain_Array classes

--- src/Arrays_and_ArrayList/BinarySearch.java
+++ src/Arrays_and_ArrayList/BinarySearch.java
@@ -1,5 +1,30 @@
 package Arrays_and_ArrayList;
 
+import java.util.*;
+
 public class BinarySearch {
-    
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] arr = { 1, 2, 3, 4, 5 };
+    int target = 2;
+    System.out.println(binary_Search(arr, target));
+  }
+
+  private static int binary_Search(int[] arr, int target) {
+    int start = 0;
+    int end = arr.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (arr[mid] == target) {
+        return mid;
+      }
+      if (target < arr[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return -1;
+  }
+
 }
--- src/Arrays_and_ArrayList/Find_in_Mountain_Array.java
+++ src/Arrays_and_ArrayList/Find_in_Mountain_Array.java
@@ -1,19 +1,19 @@
-package Arrays_and_ArrayList;
-
-import java.util.*;
-
-interface MountainArray {
-    public int get(int index){}
-
-    public int length(){}
-}
-
-public class Find_in_Mountain_Array {
-    public int findInMountainArray(int target, MountainArray mountainArr) {
-        int peak=getPeakElement(mountainArr);
-    }
-
-    int getPeakElement(MountainArray mountainArr){
-        
-    }
-}
+//package Arrays_and_ArrayList;
+//
+//import java.util.*;
+//
+//interface MountainArray {
+//    public int get(int index){}
+//
+//    public int length(){}
+//}
+//
+//public class Find_in_Mountain_Array {
+//    public int findInMountainArray(int target, MountainArray mountainArr) {
+//        int peak=getPeakElement(mountainArr);
+//    }
+//
+//    int getPeakElement(MountainArray mountainArr){
+//
+//    }
+//}
