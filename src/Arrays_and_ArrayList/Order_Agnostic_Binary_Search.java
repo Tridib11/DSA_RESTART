23b7e07a05682d295ea14124e9efb3945fe1ddd5
Tridib11 <tridibghosh12345@gmail.com>
2024-05-18 00:51:21 +0530

    Refactor Order_Agnostic_Binary_Search class for improved readability and maintainability

--- src/Arrays_and_ArrayList/Order_Agnostic_Binary_Search.java
+++ src/Arrays_and_ArrayList/Order_Agnostic_Binary_Search.java
@@ -1,37 +1,34 @@
 package Arrays_and_ArrayList;
 
 import java.util.*;
+
 public class Order_Agnostic_Binary_Search {
-  public static void main(String[] args){
-    int[] arr={2};
-    int target=2;
-    int ans=0;
-    if(arr.length==1){
-      ifJustOne(arr,target);
+  public static void main(String[] args) {
+    int[] arr = { 2 };
+    int target = 2;
+    int ans = 0;
+    if (arr.length == 1) {
+      ifJustOne(arr, target);
       return;
     }
-    if(arr[0]<arr[arr.length-1]){
-      ans=aescendingBS(arr,target);
+    if (arr[0] < arr[arr.length - 1]) {
+      ans = aescendingBS(arr, target);
+    } else {
+      ans = descendingBS(arr, target);
     }
-    else {
-      ans=descendingBS(arr,target);
-    }  
 
     System.out.println(ans);
   }
 
-
-  private static void ifJustOne(int[] arr,int target){
-    if(arr[0]==target){
+  private static void ifJustOne(int[] arr, int target) {
+    if (arr[0] == target) {
       System.out.println(0);
-    }
-    else{
+    } else {
       System.out.println(-1);
     }
   }
 
-
-  private static int aescendingBS(int[] arr,int target){
+  private static int aescendingBS(int[] arr, int target) {
     int start = 0;
     int end = arr.length - 1;
     while (start <= end) {
@@ -48,20 +45,18 @@ public class Order_Agnostic_Binary_Search {
     return -1;
   }
 
-
-  private static int descendingBS(int[] arr,int target){
-    int start=0;
-    int end=arr.length-1;
-    while(start<=end){
-      int mid=start+(end-start)/2;
-      if(arr[mid]==target){
+  private static int descendingBS(int[] arr, int target) {
+    int start = 0;
+    int end = arr.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (arr[mid] == target) {
         return mid;
       }
-      if(target<arr[mid]){
-        start=mid+1;
-      }
-      else{
-        end=mid-1;
+      if (target < arr[mid]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
       }
     }
     return -1;
