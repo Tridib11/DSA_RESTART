926f425ef22d3d666e9501e274bf3d0900d44b46
Tridib11 <tridibghosh12345@gmail.com>
2024-05-18 15:16:50 +0530

    Refactor RunManager component for improved code organization and clarity

--- src/Arrays_and_ArrayList/Floor_of_A_Number.java
+++ src/Arrays_and_ArrayList/Floor_of_A_Number.java
@@ -1,5 +1,21 @@
 package Arrays_and_ArrayList;
-
+//Greatest number samller or equal to the target
 public class Floor_of_A_Number {
-    
+  public static void main(String[] args) {
+    int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+    int target = 15;
+    System.out.println(floor(arr, target));
+  }
+
+  private static int floor(int[] arr, int target) {
+    int start=0;
+    int end=arr.length-1;
+    while(start<=end){
+      int mid=start+(end-start)/2;
+      if(arr[mid]==target) return arr[mid];
+      if(target<arr[mid]) end=mid-1;
+      else start=mid+1;
+    }
+    return arr[end];
+  }
 }
