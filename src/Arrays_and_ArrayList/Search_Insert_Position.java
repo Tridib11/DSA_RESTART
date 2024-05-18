4cb02c49ba25349086facc2933917f216b8618d7
Tridib11 <tridibghosh12345@gmail.com>
2024-05-18 12:40:49 +0530

    Refactor Search_Insert_Position class for improved readability and maintainability

--- src/Arrays_and_ArrayList/Search_Insert_Position.java
+++ src/Arrays_and_ArrayList/Search_Insert_Position.java
@@ -1,4 +1,8 @@
 package Arrays_and_ArrayList;
 //https://leetcode.com/problems/search-insert-position/
+
 public class Search_Insert_Position {
+  public static void main(String[] args) {
+    
+  }
 }
