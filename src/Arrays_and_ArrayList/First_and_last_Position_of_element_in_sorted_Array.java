28d169f43ac20840608fb977eddef9b2202b4353
Tridib11 <tridibghosh12345@gmail.com>
2024-05-18 16:07:54 +0530

    Files added

--- src/Arrays_and_ArrayList/First_and_last_Position_of_element_in_sorted_Array.java
+++ src/Arrays_and_ArrayList/First_and_last_Position_of_element_in_sorted_Array.java
@@ -1,10 +1,10 @@
 package Arrays_and_ArrayList;
-
+import java.util.*;
 public class First_and_last_Position_of_element_in_sorted_Array {
     public static void main(String[] args) {
       int[] arr={5,6,7,8,8,10};
       int target=8;
-      System.out.println(first_and_last_position(arr,target));
+      System.out.println(Arrays.toString(first_and_last_position(arr,target)));
     }
 
 
@@ -18,7 +18,7 @@ public class First_and_last_Position_of_element_in_sorted_Array {
           ans[0]=mid;
           end=mid-1;
         }
-        if(target<arr[mid]) end=mid-1;
+        else if(target<arr[mid]) end=mid-1;
         else start=mid+1;
       }
 
@@ -30,7 +30,9 @@ public class First_and_last_Position_of_element_in_sorted_Array {
           ans[1]=mid;
           start=mid+1;
         }
-        if(arr[mid]<target) 
+        else if(target<arr[mid]) end=mid-1;
+        else start=mid+1;
       }
+      return ans;
     }
 }
