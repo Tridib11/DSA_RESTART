72d3abd215f1c6f99ff67ec474a10939259f5e04
Tridib11 <tridibghosh12345@gmail.com>
2024-05-18 15:55:32 +0530

    Refactor First_and_last_Position_of_element_in_sorted_Array class for improved readability and maintainability

--- src/Arrays_and_ArrayList/First_and_last_Position_of_element_in_sorted_Array.java
+++ src/Arrays_and_ArrayList/First_and_last_Position_of_element_in_sorted_Array.java
@@ -1,5 +1,36 @@
 package Arrays_and_ArrayList;
 
 public class First_and_last_Position_of_element_in_sorted_Array {
+    public static void main(String[] args) {
+      int[] arr={5,6,7,8,8,10};
+      int target=8;
+      System.out.println(first_and_last_position(arr,target));
+    }
 
+
+    private static int[] first_and_last_position(int[] arr,int target){
+      int[] ans={-1,-1};
+      int start=0;
+      int end=arr.length-1;
+      while(start<=end){
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target){
+          ans[0]=mid;
+          end=mid-1;
+        }
+        if(target<arr[mid]) end=mid-1;
+        else start=mid+1;
+      }
+
+      start=0;
+      end=arr.length-1;
+      while(start<=end){
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target){
+          ans[1]=mid;
+          start=mid+1;
+        }
+        if(arr[mid]<target) 
+      }
+    }
 }
