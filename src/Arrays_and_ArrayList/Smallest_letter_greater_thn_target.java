e048fadc85b42f27d2b9dcf54d3b5c10022c03cf
Tridib11 <tridibghosh12345@gmail.com>
2024-05-19 12:35:07 +0530

    Refactor Smallest_letter_greater_thn_target class for improved readability and maintainability

--- src/Arrays_and_ArrayList/Smallest_letter_greater_thn_target.java
+++ src/Arrays_and_ArrayList/Smallest_letter_greater_thn_target.java
@@ -1,4 +1,15 @@
 package Arrays_and_ArrayList;
+
 //https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
 public class Smallest_letter_greater_thn_target {
+  public static void main(String[] args) {
+    char[] arr = { 'c', 'f', 'j' };
+    char target = 'c';
+    System.out.println(nextGreatestLetter(arr, target));
+  }
+
+  public static char nextGreatestLetter(char[] letters, char target) {
+
+  }
+
 }
