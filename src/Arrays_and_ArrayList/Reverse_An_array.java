a6847974aa93c82a7eca36ef2e0432cff0157452
Tridib11 <tridibghosh12345@gmail.com>
2024-06-03 06:55:08 +0530

    Refactor Reverse_An_array class for improved readability and maintainability

--- src/Arrays_and_ArrayList/Reverse_An_array.java
+++ src/Arrays_and_ArrayList/Reverse_An_array.java
@@ -1,5 +1,20 @@
 package Arrays_and_ArrayList;
 
+import java.util.Arrays;
+
 public class Reverse_An_array {
-    psv
+    public static void main(String[] args){
+      int[] arr={1,2,3,4,5,6};
+      reverse(arr);
+      Arrays.toString(arr);
+      System.out.println(arr);
+    }
+
+    private static void reverse(int[] arr){
+      int start=0;
+      int end=arr.length-1;
+      while(start<end){
+        
+      }
+    }
 }
