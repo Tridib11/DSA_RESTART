84d58028b77466794a9e61e79f349300dd327ccc
Tridib11 <tridibghosh12345@gmail.com>
2024-06-03 07:29:55 +0530

    Files added

--- src/Arrays_and_ArrayList/Count_even_number_of_digits.java
+++ src/Arrays_and_ArrayList/Count_even_number_of_digits.java
@@ -1,6 +1,29 @@
 package Arrays_and_ArrayList;
 
 public class Count_even_number_of_digits {
-  
+  public static void main(String[] args){
+    int[] arr={12,345,2,6,7896};
+    System.out.println(number_Of_Evens(arr));
+  }
+
+  private static int number_Of_Evens(int[] arr){
+    int count=0;
+    for(int num:arr){
+      if(is_Even(num)){
+        count++;
+      }
+    }
+    return count;
+  }
+
+  private static boolean is_Even(int num) {
+    int count = 0;
+    while (num > 0) {
+        count++;
+        num = num / 10;
+    }
+    return (count % 2 == 0) ? true : false;
+}
+
 }
 
