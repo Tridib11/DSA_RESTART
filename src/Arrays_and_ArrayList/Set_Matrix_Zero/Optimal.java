8a5e901b7d2fd76a3a67059573f5aff7b8250f9f
Tridib11 <tridibghosh12345@gmail.com>
2024-06-03 22:56:34 +0530

    File added

--- src/Arrays_and_ArrayList/Set_Matrix_Zero/Optimal.java
+++ src/Arrays_and_ArrayList/Set_Matrix_Zero/Optimal.java
@@ -2,40 +2,83 @@ package Arrays_and_ArrayList.Set_Matrix_Zero;
 
 public class Optimal {
     public static void main(String[] args) {
-        
+        int[][] matrix = {
+                { 1, 1, 1 },
+                { 1, 0, 1 },
+                { 1, 1, 1 }
+        };
+        setZeroes(matrix);
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
     }
 
-    private static void setZeroes(int[][] matrix){
-        int row=matrix.length;
-        int col=matrix[row].length;
-        
-        int flagFirstRow=1;
-        int flagFirstCol=1;
+    private static void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
 
-        for(int i=0;i<row;i++){
-            if(matrix[0][i]==0){
-                flagFirstRow=0;
+        int flagFirstRow = 1;
+        int flagFirstCol = 1;
+
+        // Check if the first row has any zero
+        for (int i = 0; i < col; i++) {
+            if (matrix[0][i] == 0) {
+                flagFirstRow = 0;
+                break;
             }
         }
 
-        for(int j=0;j<col;j++){
-            if(matrix[j][0]==0){
-                flagFirstCol=0;
+        // Check if the first column has any zero
+        for (int j = 0; j < row; j++) {
+            if (matrix[j][0] == 0) {
+                flagFirstCol = 0;
+                break;
             }
         }
 
-        //inner matrix
-
-        for(int i=1;i<row;i++){
-            for(int j=1;j<col;j++){
-                if(matrix[i][j]==0){
-                    matrix[0][i]=0; //changing the first row to 0
-                    matrix[j][0]=0; //changing the first col to 0 
+        // Mark zeros in the first row and column based on inner matrix zeros
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0; // Mark the corresponding first row
+                    matrix[i][0] = 0; // Mark the corresponding first column
                 }
             }
         }
 
-        
+        // Zero out rows based on marks in the first column
+        for (int i = 1; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < col; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
 
+        // Zero out columns based on marks in the first row
+        for (int j = 1; j < col; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < row; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero out the first row if needed
+        if (flagFirstRow == 0) {
+            for (int i = 0; i < col; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        // Zero out the first column if needed
+        if (flagFirstCol == 0) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
