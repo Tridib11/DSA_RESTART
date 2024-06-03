60300ee35d5b47ae30b429ade3a481a665de38ef
Tridib11 <tridibghosh12345@gmail.com>
2024-06-03 07:30:53 +0530

    Refactor Count_even_number_of_digits class for improved readability and maintainability

--- src/Arrays_and_ArrayList/Count_even_number_of_digits.java
+++ src/Arrays_and_ArrayList/Count_even_number_of_digits.java
@@ -1,15 +1,15 @@
 package Arrays_and_ArrayList;
 
 public class Count_even_number_of_digits {
-  public static void main(String[] args){
-    int[] arr={12,345,2,6,7896};
+  public static void main(String[] args) {
+    int[] arr = { 12, 345, 2, 6, 7896 };
     System.out.println(number_Of_Evens(arr));
   }
 
-  private static int number_Of_Evens(int[] arr){
-    int count=0;
-    for(int num:arr){
-      if(is_Even(num)){
+  private static int number_Of_Evens(int[] arr) {
+    int count = 0;
+    for (int num : arr) {
+      if (is_Even(num)) {
         count++;
       }
     }
@@ -19,11 +19,10 @@ public class Count_even_number_of_digits {
   private static boolean is_Even(int num) {
     int count = 0;
     while (num > 0) {
-        count++;
-        num = num / 10;
+      count++;
+      num = num / 10;
     }
     return (count % 2 == 0) ? true : false;
-}
+  }
 
 }
-
