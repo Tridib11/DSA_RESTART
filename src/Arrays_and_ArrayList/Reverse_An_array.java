dfad79635c6a354d474a65c25df25daa74047908
Tridib11 <tridibghosh12345@gmail.com>
2024-06-03 06:56:41 +0530

    Files added

--- src/Arrays_and_ArrayList/Reverse_An_array.java
+++ src/Arrays_and_ArrayList/Reverse_An_array.java
@@ -3,22 +3,22 @@ package Arrays_and_ArrayList;
 import java.util.Arrays;
 
 public class Reverse_An_array {
-    public static void main(String[] args){
-      int[] arr={1,2,3,4,5,6};
-      reverse(arr);
-      System.out.println(Arrays.toString(arr));
-    }
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5, 6 };
+    reverse(arr);
+    System.out.println(Arrays.toString(arr));
+  }
 
-    private static void reverse(int[] arr){
-      int start=0;
-      int end=arr.length-1;
-      while(start<end){
-        int temp=arr[start];
-        arr[start]=arr[end];
-        arr[end]=temp;
+  private static void reverse(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+    while (start < end) {
+      int temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
 
-        start++;
-        end--;
-      }
+      start++;
+      end--;
     }
+  }
 }
