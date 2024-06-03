4b510bd4c7a032b31e587eb6640358d0c7025259
Tridib11 <tridibghosh12345@gmail.com>
2024-06-03 07:17:29 +0530

    Files added

--- src/Arrays_and_ArrayList/Search_in_2D_Array.java
+++ src/Arrays_and_ArrayList/Search_in_2D_Array.java
@@ -1,5 +1,6 @@
 package Arrays_and_ArrayList;
 
+import java.util.*;
 public class Search_in_2D_Array {
   public static void main(String[] args) {
     int[][] arr = {
@@ -8,13 +9,19 @@ public class Search_in_2D_Array {
         { 34, 55, 66, 77 },
         { 9, 89, 0 }
     };
-    int target=3;
-    search(target,arr);
-
+    int target = 99;
+    int[] ans = search(target, arr);
+    System.out.println(Arrays.toString(ans));
   }
 
-
-  private static int[][] search(int target,int[][] arr){
-    
+  private static int[] search(int target, int[][] arr) {
+    for (int row = 0; row < arr.length; row++) {
+      for (int col = 0; col < arr[row].length; col++) {
+        if (arr[row][col] == target) {
+          return new int[] { row, col };
+        }
+      }
+    }
+    return new int[] { -1, -1 };
   }
 }
