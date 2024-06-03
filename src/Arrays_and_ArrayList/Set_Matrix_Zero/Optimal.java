2553969d1e33eef0da932dafe19081aa9e529c45
Tridib11 <tridibghosh12345@gmail.com>
2024-06-03 21:53:43 +0530

    Refactor setZeroes method for improved performance and readability

--- src/Arrays_and_ArrayList/Set_Matrix_Zero/Optimal.java
+++ src/Arrays_and_ArrayList/Set_Matrix_Zero/Optimal.java
@@ -4,4 +4,25 @@ public class Optimal {
     public static void main(String[] args) {
         
     }
+
+    private static void setZeroes(int[][] matrix){
+        int row=matrix.length;
+        int col=matrix[row].length;
+        
+        int flagFirstRow=1;
+        int flagFirstCol=1;
+
+        for(int i=0;i<row;i++){
+            if(matrix[0][i]==0){
+                flagFirstRow=0;
+            }
+        }
+
+        for(int j=0;j<col;j++){
+            if(matrix[j][0]==0){
+                flagFirstCol=0;
+            }
+        }
+
+    }
 }
