522ff803903ed5902d3c9fa6000967d73887b43b
Tridib11 <tridibghosh12345@gmail.com>
2024-06-03 07:03:44 +0530

    Files added

--- src/Arrays_and_ArrayList/Find_in_a_String.java
+++ src/Arrays_and_ArrayList/Find_in_a_String.java
@@ -1,5 +1,19 @@
 package Arrays_and_ArrayList;
-
+import java.util.*;
 public class Find_in_a_String {
+  public static void main(String[] args){
+    String s="rohan";
+    char c='x';
+    System.out.println(find(s,c));
+  }
 
+  private static boolean find(String s,char c){
+    if(s.length()==0) return false;
+    for(int i=0;i<s.length();i++){
+      if(s.charAt(i)==c){
+        return true;
+      }
+    }
+    return false;
+  }
 }
