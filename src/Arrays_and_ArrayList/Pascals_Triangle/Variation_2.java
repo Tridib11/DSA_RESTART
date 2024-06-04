51bc3911923f2d2e1efdd0669a1c52b86671e6ad
Tridib11 <tridibghosh12345@gmail.com>
2024-06-04 07:13:24 +0530

    feat: Add Variation_2 class for Pascal's Triangle

--- src/Arrays_and_ArrayList/Pascals_Triangle/Variation_2.java
+++ src/Arrays_and_ArrayList/Pascals_Triangle/Variation_2.java
@@ -1,5 +1,26 @@
 package Arrays_and_ArrayList.Pascals_Triangle;
 
+/*
+ * Print any given row and coloumn of a pascals triangle n=5
+ * 
+ * T(C)- O(n)
+ * S(C)- O(1)
+ */
+
 public class Variation_2 {
-    
+  public static void main(String[] args) {
+    int n = 5;
+    pascalsTriangle(n);
+  }
+
+  private static void pascalsTriangle(int n) {
+    long ans = 1;
+    System.out.print(ans + " ");
+
+    for (int i = 1; i < n; i++) {
+      ans = ans * (n - i);
+      ans = ans / i;
+      System.out.print(ans + " ");
+    }
+  }
 }
