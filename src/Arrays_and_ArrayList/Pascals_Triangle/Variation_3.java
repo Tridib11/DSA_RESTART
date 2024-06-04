6fc79e5c3ca65d54c52b7fc8e3ab5e6b9145c2ab
Tridib11 <tridibghosh12345@gmail.com>
2024-06-04 08:05:35 +0530

    feat: Update Pascal's Triangle configurations for Variation_3 class

--- src/Arrays_and_ArrayList/Pascals_Triangle/Variation_3.java
+++ src/Arrays_and_ArrayList/Pascals_Triangle/Variation_3.java
@@ -13,33 +13,45 @@ package Arrays_and_ArrayList.Pascals_Triangle;
  */
 
 
-import java.util.*;
+ import java.util.*;
 
-public class Variation_3 {
-  public static void main(String[] args) {
-    
-  }
-
-  private static List<List<Integer>> generate(int numRows) {
-       List<List<Integer>> ans=new ArrayList<>();
-       for(int row=1;row<=numRows;row++){
-        ans.add(generateRows(row));
-       }
-       return ans;
-  }
-
-  private static List<Integer> generateRows(int row){
-    long ans=1;
-    List<Integer> ansRow=new ArrayList<>();
-    ansRow.add(1);
-    for(int col=1;col<row;col++){
-      ans=ans*(row-col);
-      ans=ans/col;
-      ansRow.add((int)ans);
-    }
-
-    return ansRow;
-  }
-
-
-}
+ public class Variation_3 {
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         
+         // Prompt the user for the number of rows
+         System.out.print("Enter the number of rows for Pascal's Triangle: ");
+         int numRows = scanner.nextInt();
+         
+         // Generate Pascal's Triangle
+         List<List<Integer>> result = generate(numRows);
+         
+         // Display the result
+         for (List<Integer> row : result) {
+             System.out.println(row);
+         }
+         
+         scanner.close();
+     }
+ 
+     private static List<List<Integer>> generate(int numRows) {
+         List<List<Integer>> ans = new ArrayList<>();
+         for (int row = 1; row <= numRows; row++) {
+             ans.add(generateRows(row));
+         }
+         return ans;
+     }
+ 
+     private static List<Integer> generateRows(int row) {
+         long ans = 1;
+         List<Integer> ansRow = new ArrayList<>();
+         ansRow.add(1);
+         for (int col = 1; col < row; col++) {
+             ans = ans * (row - col);
+             ans = ans / col;
+             ansRow.add((int) ans);
+         }
+         return ansRow;
+     }
+ }
+ 
