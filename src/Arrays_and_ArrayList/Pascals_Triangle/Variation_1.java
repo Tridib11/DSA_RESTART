ec8db747f19a253bce73f27b9652a5eaf14eb215
Tridib11 <tridibghosh12345@gmail.com>
2024-06-04 06:46:57 +0530

    Add Variation_1 class for Pascal's Triangle

--- src/Arrays_and_ArrayList/Pascals_Triangle/Variation_1.java
+++ src/Arrays_and_ArrayList/Pascals_Triangle/Variation_1.java
@@ -1,17 +1,31 @@
 package Arrays_and_ArrayList.Pascals_Triangle;
 
-
 /*
  * Given Row and Coloumn tell me the element at R=5 and C=3 
+ * T(c)- O(r)
+ * S(c)- O(1)
  */
 
-
 public class Variation_1 {
+
   public static void main(String[] args) {
-    
-  } 
-  
-  private static int ncr(int n,int r){
-    
+    int r = 5; // row number
+    int c = 3; // col number
+    int element = pascalTriangle(r, c);
+    System.out.println("The element at position (r,c) is: " + element);
+  }
+
+  private static int pascalTriangle(int r, int c) {
+    int element = (int) nCr(r - 1, c - 1);
+    return element;
+  }
+
+  private static long nCr(int n, int r) {
+    long res = 1;
+    for (int i = 0; i < r; i++) {
+      res = res * (n - i);
+      res = res / (i + 1);
+    }
+    return res;
   }
 }
