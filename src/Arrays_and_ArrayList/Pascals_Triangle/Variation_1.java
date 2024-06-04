a60141c248511ac87399f267bb0fc52e23e22ee8
Tridib11 <tridibghosh12345@gmail.com>
2024-06-04 06:43:03 +0530

    Add Variation_1 class for Pascal's Triangle

--- src/Arrays_and_ArrayList/Pascals_Triangle/Variation_1.java
+++ src/Arrays_and_ArrayList/Pascals_Triangle/Variation_1.java
@@ -1,5 +1,17 @@
 package Arrays_and_ArrayList.Pascals_Triangle;
 
+
+/*
+ * Given Row and Coloumn tell me the element at R=5 and C=3 
+ */
+
+
 public class Variation_1 {
+  public static void main(String[] args) {
     
+  } 
+  
+  private static int ncr(int n,int r){
+    
+  }
 }
