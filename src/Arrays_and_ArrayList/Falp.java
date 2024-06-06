1be64b3bd6bc2c70680780efbd2c01f84a0b2aaf
Tridib11 <tridibghosh12345@gmail.com>
2024-06-06 07:50:16 +0530

    file added

---
+++ src/Arrays_and_ArrayList/Falp.java
@@ -0,0 +1,12 @@
+package Arrays_and_ArrayList;
+
+public class Falp {
+  public static void main(String[] args) {
+    
+  }
+
+
+  private int[] searchRange(int[] nums, int target) {
+        
+  }
+}
