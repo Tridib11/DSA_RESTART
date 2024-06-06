271787cbdee9861a3321e5ff303f823892ae49d6
Tridib11 <tridibghosh12345@gmail.com>
2024-06-06 05:32:52 +0530

    Files added

--- src/Arrays_and_ArrayList/Check_if_The_Array_is_Sorted_and_Rotated.java
+++ src/Arrays_and_ArrayList/Check_if_The_Array_is_Sorted_and_Rotated.java
@@ -1,4 +1,20 @@
 package Arrays_and_ArrayList;
-
+//https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
 public class Check_if_The_Array_is_Sorted_and_Rotated {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean check(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%nums.length]){
+                count++;
+            }
+            if(count>1){
+                return false;
+            }
+        }
+        return true;
+    }
 }
