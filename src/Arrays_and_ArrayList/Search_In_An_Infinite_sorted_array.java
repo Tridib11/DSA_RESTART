72487fda7ee6eda7e96b6ff879f83f2011577391
Tridib11 <tridibghosh12345@gmail.com>
2024-06-07 12:46:53 +0530

    feat: Refactor rangeDecider method in Search_In_An_Infinite_sorted_array class

--- src/Arrays_and_ArrayList/Search_In_An_Infinite_sorted_array.java
+++ src/Arrays_and_ArrayList/Search_In_An_Infinite_sorted_array.java
@@ -2,36 +2,33 @@ package Arrays_and_ArrayList;
 
 public class Search_In_An_Infinite_sorted_array {
   public static void main(String[] args) {
-    int[] arr={1,3,5,6,7,99,100};
-    rangeDecider(arr,3);
+    int[] arr = { 1, 3, 5, 6, 7, 99, 100 };
+    rangeDecider(arr, 3);
   }
 
-  static void rangeDecider(int[] arr,int target){
-    int start=0;
-    int end=1;
-    while(target>arr[end]){
-      int newStart=end+1;
-      end=end+(end-start+1)*2;
-      start=newStart;
+  static void rangeDecider(int[] arr, int target) {
+    int start = 0;
+    int end = 1;
+    while (target > arr[end]) {
+      int newStart = end + 1;
+      end = end + (end - start + 1) * 2;
+      start = newStart;
     }
-    System.out.println(search(arr,target,start,end));
-
+    System.out.println(search(arr, target, start, end));
 
   }
 
-
-  static int search(int[] arr,int target,int start,int end){
-    while(start<=end){
-      int mid=start+(end-start)/2;
-      if(target==arr[mid]){
+  static int search(int[] arr, int target, int start, int end) {
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (target == arr[mid]) {
         return mid;
       }
 
-      if(target<arr[mid]){
-        end=mid-1;
-      }
-      else {
-        start=mid+1;
+      if (target < arr[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
       }
     }
 
