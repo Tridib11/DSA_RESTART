cd924f3a6f00e8e29ffc618bc43438051ab0ef1a
Tridib11 <tridibghosh12345@gmail.com>
2024-06-07 13:39:29 +0530

    Refactor workspace.xml and Peak_Index_In_A_Mountain_Array.java

---
+++ src/Arrays_and_ArrayList/Find_in_Mountain_Array.java
@@ -0,0 +1,9 @@
+package Arrays_and_ArrayList;
+
+public class Find_in_Mountain_Array {
+    public static void main(String[] args) {
+      
+    }
+
+    
+}
--- src/Arrays_and_ArrayList/Peak_Index_In_A_Mountain_Array.java
+++ src/Arrays_and_ArrayList/Peak_Index_In_A_Mountain_Array.java
@@ -7,18 +7,18 @@ public class Peak_Index_In_A_Mountain_Array {
     }
 
     public static int peakIndexInMountainArray(int[] arr) {
-        int start=0;
-        int end=arr.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]>arr[mid+1]){
-                end=mid;
+            int start=0;
+            int end=arr.length-1;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(arr[mid]>arr[mid+1]){
+                    end=mid;
+                }
+                else{
+                    start=mid+1;
+                }
             }
-            else{
-                start=mid+1;
-            }
-        }
-        return start;
+            return start;
     }
 
     
