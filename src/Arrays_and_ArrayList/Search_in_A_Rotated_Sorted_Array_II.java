a7213b9fcfbadbb951c7399d87a1d8436a1838e0
Tridib11 <tridibghosh12345@gmail.com>
2024-06-07 18:12:14 +0530

    Refactor Search_In_A_Rotated_Sorted_Array class and add binary search implementation

--- src/Arrays_and_ArrayList/Search_in_A_Rotated_Sorted_Array_II.java
+++ src/Arrays_and_ArrayList/Search_in_A_Rotated_Sorted_Array_II.java
@@ -1,4 +1,22 @@
 package Arrays_and_ArrayList;
 
 public class Search_in_A_Rotated_Sorted_Array_II {
+  public static void main(String[] args) {
+
+  }
+
+  public boolean search(int[] nums, int target) {
+    int start = 0, end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] == target)
+        return true;
+      if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+        start++;
+        end--;
+      }
+
+    }
+  }
+
 }
