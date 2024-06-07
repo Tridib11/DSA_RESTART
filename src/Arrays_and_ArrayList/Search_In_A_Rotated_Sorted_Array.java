7fc176a01ae8597923eacaa6abde5c1ddb6d2a7e
Tridib11 <tridibghosh12345@gmail.com>
2024-06-07 17:55:57 +0530

    Refactor Search_In_A_Rotated_Sorted_Array class and add binary search implementation

--- src/Arrays_and_ArrayList/Search_In_A_Rotated_Sorted_Array.java
+++ src/Arrays_and_ArrayList/Search_In_A_Rotated_Sorted_Array.java
@@ -1,32 +1,31 @@
 package Arrays_and_ArrayList;
+
 //https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 public class Search_In_A_Rotated_Sorted_Array {
   public static void main(String[] args) {
-    
+
   }
 
   public static int search(int[] nums, int target) {
-        int start=0,end=nums.length-1;
-        while(start<=end){
-          int mid=start+(end-start)/2;
-          if(nums[mid]==target) return mid;
-          if(nums[start]<=nums[mid]){
-            if(nums[start]<=target && target<=nums[mid]){
-              end=mid-1;
-            }
-            else{
-              start=mid+1;
-            }
-          }
-          else{
-            if(nums[mid]<=target && target<=nums[end]){
-              start=mid+1;
-            }
-            else{
-              end=mid-1;
-            }
-          } 
+    int start = 0, end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] == target)
+        return mid;
+      if (nums[start] <= nums[mid]) {
+        if (nums[start] <= target && target <= nums[mid]) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
         }
-        return -1;
+      } else {
+        if (nums[mid] <= target && target <= nums[end]) {
+          start = mid + 1;
+        } else {
+          end = mid - 1;
+        }
+      }
+    }
+    return -1;
   }
 }
