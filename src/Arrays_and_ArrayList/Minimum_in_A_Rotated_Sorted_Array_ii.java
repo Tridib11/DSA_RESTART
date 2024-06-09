e086e8ba58c33f5bfd69cd8b541bc9fe7eadde98
Tridib11 <tridibghosh12345@gmail.com>
2024-06-10 00:05:00 +0530

    Refactor Minimum_in_A_Rotated_Sorted_Array class and add binary search implementation

--- src/Arrays_and_ArrayList/Minimum_in_A_Rotated_Sorted_Array_ii.java
+++ src/Arrays_and_ArrayList/Minimum_in_A_Rotated_Sorted_Array_ii.java
@@ -1,15 +1,23 @@
 package Arrays_and_ArrayList;
 
+import java.util.*;
 public class Minimum_in_A_Rotated_Sorted_Array_ii {
   public static void main(String[] args) {
     
   }
 
   public static int findMin(int[] nums) {
-     int start=0,end=nums.length-1;
+     int start=0,end=nums.length-1,min=Integer.MAX_VALUE;
      while(start<end){
       int mid=start+(end-start)/2;
-      
+      if(nums[start]==nums[mid] && nums[mid]==nums[end]){
+        min=Math.min(min,nums[mid]);
+        start++;
+        end--;
+      }
+      if(nums[start]<=nums[mid]){
+        
+      }
      }   
   }
 }
