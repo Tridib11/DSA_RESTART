60fd146c9802389dfc2dadac4cf91be52719e50d
Tridib11 <tridibghosh12345@gmail.com>
2024-06-13 15:32:18 +0530

    Files added

--- src/Arrays_and_ArrayList/Left_Rotate_by_D_places/bruteforce.java
+++ src/Arrays_and_ArrayList/Left_Rotate_by_D_places/bruteforce.java
@@ -4,16 +4,28 @@ import java.util.Arrays;
 
 public class bruteforce {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6,7};
-        int n=arr.length;
-        int d=3;
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int n = arr.length;
+        int d = 3;
 
-        System.out.println(Arrays.toString(rotate(arr,n,d)));
+        System.out.println(Arrays.toString(rotate(arr, n, d)));
     }
 
-    private static int[] rotate(int[] arr,int n,int d){
-        d=d%n;
-        int[] temp=new int[d];
+    private static int[] rotate(int[] arr, int n, int d) {
+        d = d % n;
+        int[] temp = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
 
+        for (int i = d; i < n; i++) {
+            arr[i - d] = arr[i];
+        }
+
+        for (int i = n - d; i < n; i++) {
+            arr[i] = temp[i - (n - d)];
+        }
+
+        return arr;
     }
 }
