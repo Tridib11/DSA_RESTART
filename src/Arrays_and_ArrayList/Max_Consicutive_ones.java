801f048d34e7d8d1e40a4e1e16b91c70339e0cb8
Tridib11 <tridibghosh12345@gmail.com>
2024-06-14 17:31:03 +0530

    Files added

--- src/Arrays_and_ArrayList/Max_Consicutive_ones.java
+++ src/Arrays_and_ArrayList/Max_Consicutive_ones.java
@@ -2,9 +2,24 @@ package Arrays_and_ArrayList;
 
 public class Max_Consicutive_ones {
     public static void main(String[] args) {
+            int[] arr={1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(arr));
 
     }
     public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxConsicutive=0,consicutive=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                consicutive+=1;
+                if(maxConsicutive<=consicutive){
+                    maxConsicutive=consicutive;
+                }
+            }
+            else{
+                consicutive=0;
+            }
 
+        }
+        return maxConsicutive;
     }
 }
