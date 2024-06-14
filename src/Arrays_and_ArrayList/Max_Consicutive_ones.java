cc75f14f1ce531c23cf3c3c2cf232a15740fe302
Tridib11 <tridibghosh12345@gmail.com>
2024-06-14 17:26:30 +0530

    Files added

---
+++ src/Arrays_and_ArrayList/Max_Consicutive_ones.java
@@ -0,0 +1,10 @@
+package Arrays_and_ArrayList;
+
+public class Max_Consicutive_ones {
+    public static void main(String[] args) {
+
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+
+    }
+}
