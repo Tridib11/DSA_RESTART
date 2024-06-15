d541ca668cc42b6d169bf415f60c5aa81dc1edfe
Tridib11 <tridibghosh12345@gmail.com>
2024-06-15 22:26:38 +0530

    Files added

--- src/Arrays_and_ArrayList/Intersection_of_two_Arrays/Optimal.java
+++ src/Arrays_and_ArrayList/Intersection_of_two_Arrays/Optimal.java
@@ -1,12 +1,18 @@
 package Arrays_and_ArrayList.Intersection_of_two_Arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Optimal {
     public static void main(String[] args) {
-
+        int[] arr1={1,2,2,1};
+        int[] arr2={2,2};
+        System.out.println(Arrays.toString(intersection(arr1,arr2)));
     }
 
-    public int[] intersection(int[] nums1, int[] nums2) {
-        int i=0,j=0;
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        int i=0,j=0,k=0;
+        int[] ans=new int[nums2.length];
         while(i<nums1.length && j< nums2.length){
             if(nums1[i]<nums2[j]){
                 i++;
@@ -15,8 +21,12 @@ public class Optimal {
                 j++;
             }
             else{
-
+                ans[k]=nums2[j];
+                k++;
+                i++;
+                j++;
             }
         }
+        return ans;
     }
 }
