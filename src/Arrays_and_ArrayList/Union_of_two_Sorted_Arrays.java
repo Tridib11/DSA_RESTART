3c0ae48263fa8fe3a10f43ce77285f0eda739b22
Tridib11 <tridibghosh12345@gmail.com>
2024-06-15 19:34:25 +0530

    chore: Refactor findUnion method in Union_of_two_Sorted_Arrays class

--- src/Arrays_and_ArrayList/Union_of_two_Sorted_Arrays.java
+++ src/Arrays_and_ArrayList/Union_of_two_Sorted_Arrays.java
@@ -12,18 +12,34 @@ public class Union_of_two_Sorted_Arrays {
         ArrayList<Integer> union = new ArrayList<>();
         int i = 0, j = 0;
         while (i < n && j < m) {
-            if (arr1[i] < arr2[j]) {
+            if (arr1[i] <= arr2[j]) {
                 if (union.size() == 0 || union.get(union.size() - 1) != arr1[i]) {
                     union.add(arr1[i]);
-                    i++;
                 }
+                i++;
             } else {
                 if (union.size() == 0 || union.get(union.size() - 1) != arr2[j]) {
                     union.add(arr2[j]);
-                    j++;
                 }
+                j++;
             }
         }
+
+        while(i<n){
+            if(union.get(union.size()-1)!=arr1[i]){
+                union.add(arr1[i]);
+            }
+            i++;
+        }
+
+        while(j<m){
+            if(union.get(union.size()-1)!=arr2[j]){
+                union.add(arr2[j]);
+            }
+            j++;
+        }
+
+        return union;
 
     }
 }
