6aa5a4ac57845c1bd1b719c29bfbb4f9725464bf
Tridib11 <tridibghosh12345@gmail.com>
2024-06-15 21:58:38 +0530

    Files added

--- src/Arrays_and_ArrayList/Intersection_of_two_Arrays/BruteForce.java
+++ src/Arrays_and_ArrayList/Intersection_of_two_Arrays/BruteForce.java
@@ -1,7 +1,31 @@
 package Arrays_and_ArrayList.Intersection_of_two_Arrays;
-
+import java.util.*;
 public class BruteForce {
     public static void main(String[] args) {
+        int[] arr1={1,2,3,3,4,5,6,7};
+        int[] arr2={3,3,4,4,5,8};
+        ArrayList<Integer> ans=intersection(arr1,arr2);
+        for(int i:ans){
+            System.out.print(i+" ");
+        }
 
+    }
+
+    private static ArrayList<Integer> intersection(int[] arr1,int[] arr2){
+        ArrayList<Integer> ans=new ArrayList<>();
+        int[] visited=new int[arr2.length];
+        for(int i=0;i<arr1.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j] && visited[j]==0){
+                    ans.add(arr2[j]);
+                    visited[j]=1;
+                    break;
+                }
+                else if(arr2[j]>arr1[i]){
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 }
