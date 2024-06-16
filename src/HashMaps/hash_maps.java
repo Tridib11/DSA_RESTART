3bc9d2f1cd8826c78f60ce2ee5148d8c73987835
Tridib11 <tridibghosh12345@gmail.com>
2024-06-16 21:32:02 +0530

    Files added

--- src/HashMaps/hash_maps.java
+++ src/HashMaps/hash_maps.java
@@ -1,7 +1,15 @@
 package HashMaps;
 
+import java.util.HashMap;
+
 public class hash_maps {
     public static void main(String[] args) {
-
+        HashMap<String,Integer> map=new HashMap<>();
+        map.put("India",120);
+        map.put("China",30);
+        map.put("US",150);
+        System.out.println(map);
+        map.put("US",190);
+        System.out.println(map);
     }
 }
