4ea04199d719671fd5afc198dc30cc3c37413543
Tridib11 <tridibghosh12345@gmail.com>
2024-06-16 21:33:47 +0530

    Files added

--- src/HashMaps/hash_maps.java
+++ src/HashMaps/hash_maps.java
@@ -11,5 +11,16 @@ public class hash_maps {
         System.out.println(map);
         map.put("US",190);
         System.out.println(map);
+
+
+        //searching
+
+        if(map.containsKey("China")){
+            System.out.println("Key contains in the map");
+        }
+
+        if(map.containsValue(30)){
+            System.out.println("value contains in the map");
+        }
     }
 }
