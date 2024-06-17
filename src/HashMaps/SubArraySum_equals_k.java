fb973df8ff67dedc336b2ea2bccfe252054903e7
Tridib11 <tridibghosh12345@gmail.com>
2024-06-17 19:31:58 +0530

    Files added

--- src/HashMaps/SubArraySum_equals_k.java
+++ src/HashMaps/SubArraySum_equals_k.java
@@ -1,5 +1,16 @@
 package HashMaps;
 
-public class SubArraySum_equals_k {
+import java.util.HashMap;
+import java.util.Map;
 
+public class SubArraySum_equals_k {
+    public static void main(String[] args) {
+
+    }
+    public static int subarraySum(int[] nums, int k) {
+        int result=0;
+        int current=0;
+        Map<Integer,Integer>map=new HashMap<>();
+
+    }
 }
