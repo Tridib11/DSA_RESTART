168e94e5256e1c57badff573969d2cca9701a877
Tridib11 <tridibghosh12345@gmail.com>
2024-06-17 22:59:26 +0530

    Files added

--- src/HashMaps/SubArraySum_equals_k.java
+++ src/HashMaps/SubArraySum_equals_k.java
@@ -11,6 +11,12 @@ public class SubArraySum_equals_k {
         int result=0;
         int current=0;
         Map<Integer,Integer>map=new HashMap<>();
-
+        map.put(0,1);
+        for(int i:nums){
+            current+=i; //getting the prefix sum
+            result+=map.getOrDefault(current-k,0);
+            map.put(current,map.getOrDefault(current-k,0)+1);
+        }
+        return result;
     }
 }
