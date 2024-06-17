68bf4590143d1f3f004f70fe8f3b1fa07821f4ae
Tridib11 <tridibghosh12345@gmail.com>
2024-06-18 00:09:43 +0530

    Files added

--- src/HashMaps/Longest_Subarray_with_Sum_K/Optimal.java
+++ src/HashMaps/Longest_Subarray_with_Sum_K/Optimal.java
@@ -1,13 +1,25 @@
 package HashMaps.Longest_Subarray_with_Sum_K;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Optimal {
     public static void main(String[] args) {
 
     }
     public static int lenOfLongSubarr (int arr[], int N, int K){
+        Map<Integer,Integer> map=new HashMap<>();
         int sum=0;
         int maxLength=0;
         for(int i=0;i<N;i++){
+            sum+=arr[i];
+            if(sum==K){
+                maxLength+=1;
+            }
+            else if()
+            if(!map.containsKey(sum)){
+                map.put(sum,i);
+            }
 
         }
     }
