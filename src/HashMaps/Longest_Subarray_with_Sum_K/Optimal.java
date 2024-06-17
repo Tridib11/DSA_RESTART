1e238b99d6dadd3f9c79525a5850550c6c2a700a
Tridib11 <tridibghosh12345@gmail.com>
2024-06-18 00:07:20 +0530

    Files added

---
+++ src/HashMaps/Longest_Subarray_with_Sum_K/Optimal.java
@@ -0,0 +1,14 @@
+package HashMaps.Longest_Subarray_with_Sum_K;
+
+public class Optimal {
+    public static void main(String[] args) {
+
+    }
+    public static int lenOfLongSubarr (int arr[], int N, int K){
+        int sum=0;
+        int maxLength=0;
+        for(int i=0;i<N;i++){
+
+        }
+    }
+}
