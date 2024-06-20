4846c76b7d73df74b0d4ea029e5a122ed66da159
Tridib11 <tridibghosh12345@gmail.com>
2024-06-21 01:13:15 +0530

    Files added

--- src/Arrays_and_ArrayList/BestTimeToBuyandSell.java
+++ src/Arrays_and_ArrayList/BestTimeToBuyandSell.java
@@ -7,6 +7,11 @@ public class BestTimeToBuyandSell {
     public int maxProfit(int[] prices) {
         int maxProfit=0;
         int minSell=prices[0];
-
+        for(int i=0;i<prices.length;i++){
+            minSell=Math.min(minSell,prices[i]);
+            int profit=prices[i]-minSell;
+            maxProfit=Math.max(maxProfit,profit);
+        }
+        return maxProfit;
     }
 }
