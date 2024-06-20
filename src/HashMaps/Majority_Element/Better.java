9b6f4d37e758fd3918505bcf2e00bd9a4fe5a697
Tridib11 <tridibghosh12345@gmail.com>
2024-06-20 14:01:35 +0530

    Files added

--- src/HashMaps/Majority_Element/Better.java
+++ src/HashMaps/Majority_Element/Better.java
@@ -1,5 +1,5 @@
 package HashMaps.Majority_Element;
-
+//https://leetcode.com/problems/majority-element/description/
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +13,11 @@ public class Better {
         int result=0;
         for(int i:nums){
             map.put(i,1+map.getOrDefault(i,0));
+            if(map.get(i)>nums.length/2){
+                result=i;
+            }
         }
+        return result;
     }
 
 }
