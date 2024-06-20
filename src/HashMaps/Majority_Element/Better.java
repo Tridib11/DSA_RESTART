09e007a8fe08efc29f69218df213e8ef06605591
Tridib11 <tridibghosh12345@gmail.com>
2024-06-20 11:40:22 +0530

    Files added

--- src/HashMaps/Majority_Element/Better.java
+++ src/HashMaps/Majority_Element/Better.java
@@ -1,8 +1,19 @@
 package HashMaps.Majority_Element;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Better {
     public static void main(String[] args) {
 
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int result=0;
+        for(int i:nums){
+            map.put(i,1+map.getOrDefault(i,0));
+        }
     }
 
 }
