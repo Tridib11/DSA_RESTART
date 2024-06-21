7e0e5c9289440151dc64d28fccc744e0857476fc
Tridib11 <tridibghosh12345@gmail.com>
2024-06-21 21:54:06 +0530

    Files added

--- src/Arrays_and_ArrayList/ReArrange_Array_Of_Elements_By_Sign/Version2.java
+++ src/Arrays_and_ArrayList/ReArrange_Array_Of_Elements_By_Sign/Version2.java
@@ -1,7 +1,7 @@
 package Arrays_and_ArrayList.ReArrange_Array_Of_Elements_By_Sign;
 
 import java.util.ArrayList;
-
+//https://www.naukri.com/code360/problems/alternate-numbers_6783445
 public class Version2 {
     public static void main(String[] args) {
 
@@ -11,8 +11,6 @@ public class Version2 {
      0 1 2 3  4 5
     -1,2,3,4,-3,1
 
-
-
     pos[]=2,3,4,1 size=4
 
     neg[]=-1,-3 size=2
@@ -20,9 +18,6 @@ public class Version2 {
     after rearanging
 
     2,-1,3,-3
-
-
-
 
      */
 
@@ -40,19 +35,26 @@ public class Version2 {
         }
         if(positive.size()>negative.size()){
 
-            for(int i=0;i<a.length;i++){
+            for(int i=0;i<negative.size();i++){
                 a[2*i]=positive.get(i);
                 a[2*i+1]=negative.get(i);
             }
             int index=negative.size()*2;
-            for(int i=negative.size()*2;i<positive.size();i++){
-
+            for(int i=negative.size();i<positive.size();i++){
+                a[index]= positive.get(i);
+                index++;
+            }
+        }else{
+            for(int i=0;i<positive.size();i++){
+                a[2*i]=positive.get(i);
+                a[2*i+1]=negative.get(i);
+            }
+            int index=positive.size()*2;
+            for(int i=positive.size();i<negative.size();i++){
+                a[index]=negative.get(i);
+                index++;
             }
         }
-
-
-
-
-
+        return a;
     }
 }
