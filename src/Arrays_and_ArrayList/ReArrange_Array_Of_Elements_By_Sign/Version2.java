2ca323a679c97bd5c12529a3522756f573b0f642
Tridib11 <tridibghosh12345@gmail.com>
2024-06-21 21:43:56 +0530

    Files added

--- src/Arrays_and_ArrayList/ReArrange_Array_Of_Elements_By_Sign/Version2.java
+++ src/Arrays_and_ArrayList/ReArrange_Array_Of_Elements_By_Sign/Version2.java
@@ -7,6 +7,25 @@ public class Version2 {
 
     }
 
+    /*
+     0 1 2 3  4 5
+    -1,2,3,4,-3,1
+
+
+
+    pos[]=2,3,4,1 size=4
+
+    neg[]=-1,-3 size=2
+
+    after rearanging
+
+    2,-1,3,-3
+
+
+
+
+     */
+
     public static int[] alternateNumbers(int []a) {
         // Write your code here.
         ArrayList<Integer> positive=new ArrayList<>();
@@ -19,6 +38,20 @@ public class Version2 {
                 negative.add(i);
             }
         }
+        if(positive.size()>negative.size()){
+
+            for(int i=0;i<a.length;i++){
+                a[2*i]=positive.get(i);
+                a[2*i+1]=negative.get(i);
+            }
+            int index=negative.size()*2;
+            for(int i=negative.size()*2;i<positive.size();i++){
+
+            }
+        }
+
+
+
 
 
     }
