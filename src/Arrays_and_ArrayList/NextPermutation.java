8d680e0ab1ac09f4fc6223f93b1829bb454eeda8
Tridib11 <tridibghosh12345@gmail.com>
2024-06-22 11:15:47 +0530

    Files added

--- src/Arrays_and_ArrayList/NextPermutation.java
+++ src/Arrays_and_ArrayList/NextPermutation.java
@@ -6,6 +6,28 @@ public class NextPermutation {
     }
 
     public void nextPermutation(int[] nums) {
+        int index=-1;
+        for(int i=nums.length-2;i>0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
 
+        if(index==-1){
+            reverse(nums,0,nums.length-1);
+        }
+
+
+    }
+
+    private void reverse(int[] arr,int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
     }
 }
