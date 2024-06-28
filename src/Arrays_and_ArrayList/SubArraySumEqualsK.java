b0df2b64bf67fc97bba8698d1a849159538a0abc
Tridib11 <tridibghosh12345@gmail.com>
2024-06-28 23:30:15 +0530

    Files added

--- src/Arrays_and_ArrayList/SubArraySumEqualsK.java
+++ src/Arrays_and_ArrayList/SubArraySumEqualsK.java
@@ -1,10 +1,15 @@
 package Arrays_and_ArrayList;
-
+import java.util.*;
 public class SubArraySumEqualsK {
     public static void main(String[] args) {
 
     }
     public int subarraySum(int[] nums, int k) {
+        int count=0,prefixSum=0;
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,1); // adding the first value
+        for(int i=0;i<=nums.length;i++){
 
+        }
     }
 }
