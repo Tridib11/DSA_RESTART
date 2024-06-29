1e6348079224702b42505f32c169b7d9574725e7
Tridib11 <tridibghosh12345@gmail.com>
2024-06-30 02:35:29 +0530

    Files added

--- src/Arrays_and_ArrayList/PascalsTriangle_revision/Version2.java
+++ src/Arrays_and_ArrayList/PascalsTriangle_revision/Version2.java
@@ -1,14 +1,20 @@
 package Arrays_and_ArrayList.PascalsTriangle_revision;
-
+//print the entire row
 public class Version2 {
     public static void main(String[] args) {
-
+        int n=5;
+        pascalsTriangle(n);
     }
 
-    private void pascalsTriangle(int n){
+    private static void pascalsTriangle(int n){
         long ans=1;
-        for(int i=0;i<n;i++){
-
+        System.out.print(ans+" ");
+        for(int col=1;col<n;col++){
+            ans=ans*(n-col);
+            ans=ans/col;
+            System.out.print(ans+" ");
         }
+        System.out.println();
+
     }
 }
