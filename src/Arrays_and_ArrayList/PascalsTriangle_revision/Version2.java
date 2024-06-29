3ee3c1c2affc4012f909acca1f8a4e2f349c28ff
Tridib11 <tridibghosh12345@gmail.com>
2024-06-30 02:04:52 +0530

    Files added

--- src/Arrays_and_ArrayList/PascalsTriangle_revision/Version2.java
+++ src/Arrays_and_ArrayList/PascalsTriangle_revision/Version2.java
@@ -1,4 +1,14 @@
 package Arrays_and_ArrayList.PascalsTriangle_revision;
 
 public class Version2 {
+    public static void main(String[] args) {
+
+    }
+
+    private void pascalsTriangle(int n){
+        long ans=1;
+        for(int i=0;i<n;i++){
+
+        }
+    }
 }
