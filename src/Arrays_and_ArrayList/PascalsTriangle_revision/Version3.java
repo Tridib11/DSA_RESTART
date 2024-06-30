8a940a35bb8765704ebc531c6d0f2168524ab31d
Tridib11 <tridibghosh12345@gmail.com>
2024-06-30 11:54:13 +0530

    Files added

---
+++ src/Arrays_and_ArrayList/PascalsTriangle_revision/Version3.java
@@ -0,0 +1,10 @@
+package Arrays_and_ArrayList.PascalsTriangle_revision;
+import java.util.*;
+public class Version3 {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> generate(int numRows) {
+
+    }
+}
