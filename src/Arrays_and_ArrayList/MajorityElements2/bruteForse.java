29702964c654cc135f3a9c14365e1148d71b962b
Tridib11 <tridibghosh12345@gmail.com>
2024-07-01 18:33:56 +0530

    Files added

---
+++ src/Arrays_and_ArrayList/MajorityElements2/bruteForse.java
@@ -0,0 +1,35 @@
+package Arrays_and_ArrayList.MajorityElements2;
+import java.util.*;
+public class bruteForse {
+    public static void main(String args[]) {
+        int[] arr = {11, 33, 33, 11, 33, 11};
+        List<Integer> ans = majorityElement(arr);
+        System.out.print("The majority elements are: ");
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static List<Integer> majorityElement(int[] nums) {
+        List<Integer> ls = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(ls.size()==0 || ls.get(0)!=nums[i]){
+                int count=0;
+                for(int j=0;j<nums.length;j++){
+                    if(nums[i]==nums[j]){
+                        count++;
+                    }
+                }
+                if(count>nums.length/3){
+                    ls.add(nums[i]);
+                }
+
+                if(ls.size()==2){
+                    break;
+                }
+            }
+        }
+        return ls;
+    }
+}
