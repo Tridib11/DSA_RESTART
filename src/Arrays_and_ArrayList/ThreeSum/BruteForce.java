f80e12663ebd376aa95678ec4624618d65d8aef8
Tridib11 <tridibghosh12345@gmail.com>
2024-07-04 15:51:39 +0530

    Files added

--- src/Arrays_and_ArrayList/ThreeSum/BruteForce.java
+++ src/Arrays_and_ArrayList/ThreeSum/BruteForce.java
@@ -1,7 +1,10 @@
 package Arrays_and_ArrayList.ThreeSum;
-
+import java.util.*;
 public class BruteForce {
     public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
 
     }
 
