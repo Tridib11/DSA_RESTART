37c7298e68f869dc575e5673cf08c6420d78b8f0
Tridib11 <tridibghosh12345@gmail.com>
2024-07-07 01:07:20 +0530

    Files added

--- src/Arrays_and_ArrayList/Four_Sum/Better.java
+++ src/Arrays_and_ArrayList/Four_Sum/Better.java
@@ -5,6 +5,24 @@ public class Better {
 
     }
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
+        Set<List<Integer>> st = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                Set<Long> hashSet=new HashSet<>();
+                for(int k=j+1;k<nums.length;k++){
+                    long sum=nums[i]+nums[j];
+                    sum+=nums[k];
+                    long minusedValue=target-sum;
+                    if(hashSet.contains(minusedValue)){
+                        List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],(int)minusedValue);
+                        Collections.sort(temp);
+                        st.add(temp);
+                    }
+                    hashSet.add((long)nums[k]);
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
     }
 }
