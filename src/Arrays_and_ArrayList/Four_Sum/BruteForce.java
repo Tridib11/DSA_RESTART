6e21c80aa3b89d75a959c1caf00077bf7e4e931a
Tridib11 <tridibghosh12345@gmail.com>
2024-07-07 00:54:32 +0530

    Files added

--- src/Arrays_and_ArrayList/Four_Sum/BruteForce.java
+++ src/Arrays_and_ArrayList/Four_Sum/BruteForce.java
@@ -2,9 +2,30 @@ package Arrays_and_ArrayList.Four_Sum;
 import java.util.*;
 public class BruteForce {
     public static void main(String[] args) {
-
+       int[] nums={1,0,-1,0,-2,2};
+       int target=0;
+       List<List<Integer>> bruteforceResult=fourSum(nums,target);
+        System.out.println(bruteforceResult);
     }
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-
+    public static  List<List<Integer>> fourSum(int[] nums, int target) {
+        Set<List<Integer>> set=new HashSet<>();
+        for(int i =0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                for(int k=j+1;k<nums.length;k++){
+                    for(int l=k+1;l<nums.length;l++){
+                        long sum=(long) nums[i]+nums[j];
+                        sum+=nums[k];
+                        sum+=nums[l];
+                        if(sum==target){
+                            List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                            Collections.sort(temp);
+                            set.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+        List<List<Integer>> ans=new ArrayList<>(set);
+        return ans;
     }
 }
