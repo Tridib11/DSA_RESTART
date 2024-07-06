b19342b0cf93ec5ea28afcebd6d4911f2eb575be
Tridib11 <tridibghosh12345@gmail.com>
2024-07-07 02:42:03 +0530

    Files added

---
+++ src/Arrays_and_ArrayList/LargestSUbArrayWith0SUm.java
@@ -0,0 +1,25 @@
+package Arrays_and_ArrayList;
+import java.util.*;
+public class LargestSUbArrayWith0SUm {
+    public static void main(String[] args) {
+
+    }
+
+    int maxLen(int arr[], int n)
+    {
+        // Your code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        int maxSum=0;
+        map.put(0, -1);
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(map.get(sum)!=null){
+                maxSum=Math.max(maxSum,i-map.get(sum));
+            }else{
+                map.put(sum,i);
+            }
+        }
+        return maxSum;
+    }
+}
