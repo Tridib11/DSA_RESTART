58008b5b05f042df84a94aa4d0b6cd7e5ca8b3c1
Tridib11 <tridibghosh12345@gmail.com>
2024-07-09 18:22:50 +0530

    Files added

--- src/Arrays_and_ArrayList/MapOverLappingIntervals/Testing.java
+++ src/Arrays_and_ArrayList/MapOverLappingIntervals/Testing.java
@@ -2,7 +2,7 @@ package Arrays_and_ArrayList.MapOverLappingIntervals;
 
 import java.util.*;
 
-public class BruteForce {
+public class Testing {
     public static void main(String[] args) {
         int[][] arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
         Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
