8c9e25bf751766f42e17b3c4d7a4c2b49dcb45da
Tridib11 <tridibghosh12345@gmail.com>
2024-07-09 18:40:43 +0530

    Files added

--- src/Arrays_and_ArrayList/MapOverLappingIntervals/BruteForceReturnList.java
+++ src/Arrays_and_ArrayList/MapOverLappingIntervals/BruteForceReturnList.java
@@ -2,7 +2,7 @@ package Arrays_and_ArrayList.MapOverLappingIntervals;
 import java.util.*;
 public class BruteForceReturnList {
     public static void main(String[] args) {
-
+//https://www.naukri.com/code360/problems/merge-all-overlapping-intervals_6783452
     }
     public static List< List< Integer > > mergeOverlappingIntervals(int [][]arr){
         Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
@@ -10,9 +10,22 @@ public class BruteForceReturnList {
         for(int i=0;i<arr.length;i++){
             int start=arr[i][0];
             int end=arr[i][1];
-            for(int j=i+1;j<arr.length;j++){
-                if(arr)
+
+            //skip all the merged elements
+
+            if(!ans.isEmpty() && end<=ans.get(ans.size()-1).get(1)){
+                continue;
             }
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j][0]<=end){
+                    end=Math.max(end,arr[j][1]);
+                }
+                else{
+                    break;
+                }
+            }
+            ans.add(Arrays.asList(start,end));
         }
+        return ans;
     }
 }
