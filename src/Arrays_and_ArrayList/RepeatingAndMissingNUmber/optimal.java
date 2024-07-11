6e3d674888bcea1a3db16ed4fef51e6906ebd52a
Tridib11 <tridibghosh12345@gmail.com>
2024-07-11 21:57:48 +0530

    Files added

---
+++ src/Arrays_and_ArrayList/RepeatingAndMissingNUmber/optimal.java
@@ -0,0 +1,10 @@
+package Arrays_and_ArrayList.RepeatingAndMissingNUmber;
+
+public class optimal {
+    public static void main(String[] args) {
+
+    }
+    public int[] findTwoElement(int arr[], int n){
+
+    }
+}
