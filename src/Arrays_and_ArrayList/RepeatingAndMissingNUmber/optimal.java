e0f5b3b929099b7ea562f4c0bd27404ae422ca6d
Tridib11 <tridibghosh12345@gmail.com>
2024-07-11 22:05:32 +0530

    Files added

--- src/Arrays_and_ArrayList/RepeatingAndMissingNUmber/optimal.java
+++ src/Arrays_and_ArrayList/RepeatingAndMissingNUmber/optimal.java
@@ -5,6 +5,14 @@ public class optimal {
 
     }
     public int[] findTwoElement(int arr[], int n){
+        long sN=n*(n+1)/2;
+        long s2N=(n * (n + 1) * (2 * n + 1)) / 6;
 
+        //calculating sum of the array
+        int s=0,s2=0;
+        for(int i=0;i<n;i++){
+            s+=arr[i];
+            s+=(long)arr[i]*(long)arr[i];
+        }
     }
 }
